135fbfb17fc8e50e81f3d1c4910e6fd603ca6edc
Replit user <>
2023-06-03 20:30:11 +0000

    changed normal passwords to the UUID 5 version

--- app.py
+++ app.py
@@ -3,15 +3,14 @@ import random
 def GeneratePassword():
     field = Element('passw-out') #init the password field
     passwrd = ""
-    chars = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','#','@','_','~','+','/']
-    for x in range(0, 256): #scramble 255 times
-        passwrd = ""
-        for i in range(0, 3):
-            for j in range(0, 4):
-                passwrd += chars[random.randint(0, len(chars) - 1)]
-
-            passwrd += "-"
-    for j in range(0, 4):
-                passwrd += chars[random.randint(0, len(chars) - 1)]
-
+    chars = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f']
+    for j in range(0, 8):
+        passwrd += chars[random.randint(0, len(chars) - 1)]
+    for j in range(0, 3):
+                passwrd += "-"
+                for i in range(0, 4):
+                    passwrd += chars[random.randint(0, len(chars) - 1)]
+    passwrd += "-"
+    for j in range(0, 12):
+        passwrd += chars[random.randint(0, len(chars) - 1)]
     field.element.innerText = passwrd #display the password
